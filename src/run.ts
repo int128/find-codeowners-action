0b2f99e5e018b506f84e1920f8745273fe9089c5
Hidetake Iwata <int128@gmail.com>
2024-03-29 16:39:53 +0900

    Initial import

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,45 @@
 import * as core from '@actions/core'
+import { RuleSet, parse } from './codeowners.js'
+import * as fs from 'fs/promises'
 
 type Inputs = {
-  name: string
+  codeowners: string
+  findByPath: string
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+type Outputs = {
+  firstOwner: string
+  owners: string[]
+  teamOwners: string[]
+  teamOwnersWithoutOrganization: string[]
 }
+
+export const run = async (inputs: Inputs): Promise<Outputs> => {
+  core.info(`Reading ${inputs.codeowners}`)
+  const content = (await fs.readFile(inputs.codeowners)).toString()
+  const rules = parse(content)
+  core.startGroup(`Parsed ${inputs.codeowners}`)
+  core.info(JSON.stringify(rules, undefined, 2))
+  core.endGroup()
+  const ruleSet = new RuleSet(rules)
+
+  const owners = ruleSet.findOwners(inputs.findByPath)
+  core.info(`Owners = ${JSON.stringify(owners, undefined, 2)}`)
+  return formatOutputs(owners)
+}
+
+export const formatOutputs = (owners: string[]): Outputs => {
+  const teams = findTeams(owners)
+  return {
+    firstOwner: firstOrEmpty(owners),
+    owners: owners,
+    teamOwners: teams,
+    teamOwnersWithoutOrganization: formatTeamsWithoutOrganization(teams),
+  }
+}
+
+const firstOrEmpty = (a: string[]): string => (a.length > 0 ? a[0] : '')
+
+const findTeams = (owners: string[]): string[] => owners.filter((owner) => owner.match(/^@.+\/.+/))
+
+const formatTeamsWithoutOrganization = (teams: string[]) => teams.map((team) => team.replace(/^@.+\/(.+)/, '@$1'))
--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,5 +1,23 @@
-import { run } from '../src/run.js'
+import { formatOutputs } from '../src/run.js'
 
-test('run successfully', async () => {
-  await expect(run({ name: 'foo' })).resolves.toBeUndefined()
+describe('formatOutputs', () => {
+  it('should format an empty owners', () => {
+    const outputs = formatOutputs([])
+    expect(outputs).toStrictEqual({
+      firstOwner: '',
+      owners: [],
+      teamOwners: [],
+      teamOwnersWithoutOrganization: [],
+    })
+  })
+
+  it('should format the owners', () => {
+    const outputs = formatOutputs(['@global-owner1', '@octo-org/octocats', 'docs@example.com'])
+    expect(outputs).toStrictEqual({
+      firstOwner: '@global-owner1',
+      owners: ['@global-owner1', '@octo-org/octocats', 'docs@example.com'],
+      teamOwners: ['@octo-org/octocats'],
+      teamOwnersWithoutOrganization: ['@octocats'],
+    })
+  })
 })
