3ae93a390e8176526deba9761b777b9783a3791c
Hidetake Iwata <int128@gmail.com>
2025-06-04 18:04:49 +0900

    Refactor codeowners (#64)

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,4 @@
-import { RuleSet, parse } from './codeowners.js'
+import { createMatcher } from './codeowners.js'
 import * as core from '@actions/core'
 import * as fs from 'fs/promises'
 import { glob } from 'glob'
@@ -14,14 +14,14 @@ type Outputs = OwnersOutputs & {
 }
 
 export const run = async (inputs: Inputs): Promise<Outputs> => {
-  const ruleSet = await readCodeowners(inputs.codeowners)
+  const codeownersMatcher = createMatcher(await fs.readFile(inputs.codeowners, 'utf8'))
   const files = inputs.pathGlob ? await expandPaths(inputs.paths) : inputs.paths
 
   const orphanFiles: string[] = []
   const owners = [
     ...new Set(
       files.flatMap((file) => {
-        const owners = ruleSet.findOwners(file)
+        const owners = codeownersMatcher.findOwners(file)
         if (owners.length > 0) {
           core.info(`File ${file} is owned by ${owners.join(' ')}`)
         } else {
@@ -37,13 +37,6 @@ export const run = async (inputs: Inputs): Promise<Outputs> => {
     ...formatOwners(owners),
     orphanFiles,
   }
-}
-
-const readCodeowners = async (path: string) => {
-  core.info(`Reading ${path}`)
-  const content = await fs.readFile(path)
-  const rules = parse(content.toString())
-  return new RuleSet(rules)
 }
 
 const expandPaths = async (paths: string[]) => {
